6cb98f43e80465d5b4aa0c9886bb9b6d7de92faf
Csizmadia Csaba <cscsabi2001@gmail.com>
2021-03-09 17:26:14 +0100

    Uploading ch15 drills

---
+++ drills/fourth/main_class.cpp
@@ -0,0 +1,69 @@
+#include "../std_lib_facilities.h"
+
+struct Person
+{
+	Person() {}
+	Person(string ff, string ll, int aa);
+
+	string first_name() const { return f; }
+	string last_name() const { return l; }
+	int age() const { return a; }
+
+private:
+	string f;
+	string l;
+	int a;
+};
+
+Person::Person(string ff, string ll, int aa)
+	: f{ff}, l{ll}, a{aa}
+{
+	if (aa < 0 || aa > 150) error("Invalid age");
+	string name = ff + ll;
+	for ( char c : name ){
+		switch(c){
+			case ';': case ':': case '"': case '\'': case '[': case ']': case '*': 
+			case '&': case '^': case '%': case '$': case '#': case '@': case '!':
+			error("Invalid character(s)");
+		}
+
+	}
+}
+
+istream& operator>>(istream& is, Person& p)
+{
+	string f;
+	string l;
+	int a;
+	is >> f >> l >> a;
+
+	if(!is) error("input error...");
+
+	p = Person(f, l, a);
+
+	return is;
+}
+
+ostream& operator<<(ostream& os, Person& p)
+{
+	return os << p.first_name() << ' ' << p.last_name() << ' ' << p.age();
+}
+
+int main()
+{	
+	//Person p{"Goofy", 63};
+	//cout << p << endl;
+
+	vector<Person> per;
+
+	cout << "Enter firstname, lastname and age! To end enter '|' instead of name" << endl;
+
+	for ( Person p; cin >> p; ){
+		if (p.first_name()=="|") break;
+		per.push_back(p);
+	}
+
+	for ( Person p : per ){
+		cout << p << endl;
+	}
+}
